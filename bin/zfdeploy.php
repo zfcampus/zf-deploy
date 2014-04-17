5b50c26bc25ff67702a6f4366bad367c6b7e4913
Matthew Weier O'Phinney <matthew@zend.com>
2014-04-17 10:50:21 -0500

    autoloading does not work in vendor deps
    Essentially, performing `require __DIR__ . '/../vendor/autoload.php';` in
    `bin/zfdeploy.php` does not work when installed as a dependency in another
    project.
    
    Added sniffs for the composer autoloader in `bin/deploy.php`, checking both
    against the sibling vendor directory, as well as the project; raises an
    exception on failure.

--- bin/zfdeploy.php
+++ bin/zfdeploy.php
@@ -5,5 +5,21 @@
  * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
-require __DIR__ . '/../vendor/autoload.php';
+switch (true) {
+    case (file_exists(__DIR__ . '/../vendor/autoload.php')):
+        // Installed standalone
+        require __DIR__ . '/../vendor/autoload.php';
+        break;
+    case (file_exists(__DIR__ . '/../../../autoload.php')):
+        // Installed as a Composer dependency
+        require __DIR__ . '/../../../autoload.php';
+        break;
+    case (file_exists('vendor/autoload.php')):
+        // As a Composer dependency, relative to CWD
+        require 'vendor/autoload.php';
+        break;
+    default:
+        throw new RuntimeException('Unable to locate Composer autoloader; please run "composer install".');
+}
+if (file_exists(__DIR__))
 include __DIR__ . '/../src/zfdeploy.php';

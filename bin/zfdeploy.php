de4510c00e88d2ddbbae17cd82b6148b4045f044
Matthew Weier O'Phinney <matthew@zend.com>
2014-04-17 09:52:08 -0500

    New "launcher" script
    - Loads autoloader
    - Includes src script

---
+++ bin/zfdeploy.php
@@ -0,0 +1,9 @@
+#!/usr/bin/env php
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+include __DIR__ . '/../src/zfdeploy.php';
